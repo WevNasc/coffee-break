35ec35667c27c868dd81dac681b2956ba8f8ede9
Weverson Nascimento <weverson.sn@gmail.com>
2020-05-29 17:50:05 -0300

    Add typography component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,19 @@ import React from "react"
 import Container from "../components/container";
 import Layout from "../components/layout";
 import Card from "../components/card";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
     <Layout>
       <Container>
-        <h1>Coffee Break</h1>
+        <Navbar />
+        <Card title="What is a web server?" 
+              body="The web server is an application that can deliver web content like HTML, images, JSON, etc... The web servers use the HTTP protocol to deliver the content which can be static or dynamic."
+              date="27 may, 2020" />
+        <Card title="What is a web server?" 
+              body="The web server is an application that can deliver web content like HTML, images, JSON, etc... The web servers use the HTTP protocol to deliver the content which can be static or dynamic."
+              date="27 may, 2020" />
         <Card title="What is a web server?" 
               body="The web server is an application that can deliver web content like HTML, images, JSON, etc... The web servers use the HTTP protocol to deliver the content which can be static or dynamic."
               date="27 may, 2020" />
