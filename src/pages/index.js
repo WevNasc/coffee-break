4684c7479a5bf78b2136c529c490f74536b60b2c
Weverson Nascimento <weverson.sn@gmail.com>
2020-05-29 16:39:42 -0300

    Add container component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
+import Container from "../components/container";
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container>
+      <h1>Coffee Break</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </Container>
+  )
 }
