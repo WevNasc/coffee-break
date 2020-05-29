52b96a97f43b275e903aaa26b248fcc1c8cdafb9
Weverson Nascimento <weverson.sn@gmail.com>
2020-05-29 17:48:44 -0300

    Add navbar component

---
+++ src/components/navbar/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import navbarStyles from "./navbar.module.css"
+
+export default function Navbar() {
+  return (
+    <nav className={navbarStyles.navbar} >
+      <a href="/" className={navbarStyles.title}>Coffee Break</a>
+      <a href="https://github.com/wevnasc/coffee-break" className={navbarStyles.button}>github</a>
+    </nav>
+  )
+}
